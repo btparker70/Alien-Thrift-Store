36efadf554bbd7c44740a5a40d4f760b98f522a7
Brian Parker <btparker70@gmail.com>
2021-12-07 23:39:27 -0800

    added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import Header from './components/header'
+import Footer from './components/footer'
+
 function App() {
   return (
     <div>
       My App
+      <Header />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
