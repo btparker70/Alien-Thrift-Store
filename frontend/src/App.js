7828a042e911cbd6ec7df9f612eb1b4af5dc3b1c
Brian Parker <btparker70@gmail.com>
2021-12-07 23:54:39 -0800

    added imports to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import Header from './components/header'
-import Footer from './components/footer'
+import './App.css'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
   return (
     <div>
-      My App
       <Header />
+      <main>
+        <h1>Welcome</h1>
+      </main>
       <Footer />
     </div>
   );
