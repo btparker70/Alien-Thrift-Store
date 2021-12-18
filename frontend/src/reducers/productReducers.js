e38071d7bcb63501e56ca11b98011416b90a5f49
Brian Parker <btparker70@gmail.com>
2021-12-18 00:05:45 -0800

    added product action, reducer and constants for redux product state

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,26 @@
+// a reducer is a function that takes in
+// the current state and an action of
+// what is wanted to be done to the state
+// then it changes and manipulates the state
+// returns new copy of state into store
+import {
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_FAIL
+} from '../constants/productConstants'
+
+export const productListReducer = (state = {products: [],}, action) => {
+  switch(action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { loading: true, product: [] }
+      
+    case PRODUCT_LIST_SUCCESS:
+      return { loading: false, product: action.payload }
+
+    case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    
+    default: 
+      return state
+  }
+}
