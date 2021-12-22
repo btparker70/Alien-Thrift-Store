c19c32335880c676b7a722498df2292ec002f34c
Brian Parker <btparker70@gmail.com>
2021-12-22 01:24:15 -0800

    added product details to actions

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 import {
+  // ALL PRODUCT DATA
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+
+  // SPECIFIC PRODUCT DATA
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
 } from "../constants/productConstants";
 
-// thunk allows double function
+// ALL PRODUCT DATA
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
@@ -19,6 +25,28 @@ export const listProducts = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+// SPECIFIC PRODUCT DATA
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
