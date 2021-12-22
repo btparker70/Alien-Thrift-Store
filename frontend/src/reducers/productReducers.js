affc43e86f3111217734fda7afa0c44257bc2248
Brian Parker <btparker70@gmail.com>
2021-12-22 01:15:42 -0800

    added product details to reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -4,11 +4,18 @@
 // then it changes and manipulates the state
 // returns new copy of state into store
 import {
+  // ALL PRODUCT DATA
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL
+  PRODUCT_LIST_FAIL,
+
+  // SPECIFIC PRODUCT DATA
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL
 } from '../constants/productConstants'
 
+// ALL PRODUCT DATA
 export const productListReducer = (state = {products: [],}, action) => {
   switch(action.type) {
     case PRODUCT_LIST_REQUEST:
@@ -18,6 +25,23 @@ export const productListReducer = (state = {products: [],}, action) => {
       return { loading: false, products: action.payload }
 
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload }
+    
+    default: 
+      return state
+  }
+}
+
+// SPECIFIC PRODUCT DATA
+export const productDetailsReducer = (state = { product: {reviews: []} }, action) => {
+  switch(action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state }
+      
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
+
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     
     default: 
