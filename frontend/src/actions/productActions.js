dfc32a7ced3249ce68c8b6f6286ee0d709aa505d
Brian Parker <btparker70@gmail.com>
2021-12-21 23:50:48 -0800

    fixed typo on productreducer. added redux dispatch and selector to homepage. added loading and error handling to homepage

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -6,7 +6,7 @@ import {
 } from "../constants/productConstants";
 
 // thunk allows double function
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
 
--- frontend/src/pages/homepage/HomePage.js
+++ frontend/src/pages/homepage/HomePage.js
@@ -1,37 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import { Row, Col,  } from 'react-bootstrap'
-// import products from '../../products'
-import Product from '../../components/product/Product'
-import axios from 'axios'
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../../components/product/Product";
+import { listProducts } from "../../actions/productActions";
 
 const HomePage = () => {
-  const [ products, setProducts ] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { error, loading, products } = productList;
 
   useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    async function fetProducts() {
-
-      const { data } = await axios.get('/api/products/');
-    setProducts(data)
-    }
-
-    fetProducts();
-
-  }, [])
 
   return (
     <div>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product, index) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}/>
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <div>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
