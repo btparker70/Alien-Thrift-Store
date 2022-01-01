fe730d5c66b05cba6c58818a878aad44ce87f171
Brian Parker <btparker70@gmail.com>
2021-12-31 19:29:57 -0800

    added remove cart item to cartreducer page and cardaction page. added dispatch function to cart page

--- frontend/src/pages/cartpage/CartPage.js
+++ frontend/src/pages/cartpage/CartPage.js
@@ -3,7 +3,7 @@ import { Link, useParams, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector  } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
 import Message from '../../components/message/Message'
-import { addToCart } from '../../actions/cartActions'
+import { addToCart, removeFromCart } from '../../actions/cartActions'
 
 
 const CartPage = () => {
@@ -27,7 +27,7 @@ const CartPage = () => {
   }, [dispatch, id, qty])
 
   const removeFromCartHandler = (id) => {
-    console.log('remove: ', id)
+    dispatch(removeFromCart(id))
   }
 
   const checkoutHandler = () => {
