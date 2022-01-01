03632079662a9fb0aea0e3b2b4cd8892eb8f582b
Brian Parker <btparker70@gmail.com>
2021-12-31 16:04:22 -0800

    added list of cart items

--- frontend/src/pages/cartpage/CartPage.js
+++ frontend/src/pages/cartpage/CartPage.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { Link, useParams, useNavigate, useLocation } from "react-router-dom";
 import { useDispatch, useSelector  } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
-import { Message } from '../../components/message/Message'
+import Message from '../../components/message/Message'
 import { addToCart } from '../../actions/cartActions'
 
 
@@ -16,7 +16,6 @@ const CartPage = () => {
 
   const cart = useSelector(state => state.cart)
   const { cartItems } = cart
-  console.log('cartItems: ', cartItems)
 
   useEffect(() => {
     if (id) {
@@ -25,9 +24,38 @@ const CartPage = () => {
   }, [dispatch, id, qty])
 
   return (
-    <div>
-      Cart
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message variant='info'>
+            Your cart is empty <Link to='/' >Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map(item => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+
+                  <Col md={2}>
+                    ${item.price}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+
+      <Col md={4}>
+      </Col>
+    </Row>
   )
 }
 
