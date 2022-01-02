9fb8bbf9e6f50264bc4a547af771f7ccbb738e67
Brian Parker <btparker70@gmail.com>
2022-01-02 00:29:23 -0800

    added framework stuff for jwt in settings.py. added url route for users/login

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,10 +1,14 @@
 # this file connects the views to urls
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 
 # when you go to these routes, load these views
 urlpatterns = [
   # homepage
+  path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
   path('', views.getRoutes, name="routes"),
   path('products/', views.getProducts, name="products"),
   path('products/<str:pk>', views.getProduct, name="product"),
