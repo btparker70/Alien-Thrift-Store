962cf02f92c618482c071dfdecc53c79c893d513
Brian Parker <btparker70@gmail.com>
2022-01-02 12:04:11 -0800

    fixed bug added line. django login now sees more user data from the tokens

--- backend/base/views.py
+++ backend/base/views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer, UserSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 # Create your views here.
 # views are key components of the application within django
@@ -19,8 +19,13 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
   def validate(self, attrs):
     data = super().validate(attrs)
 
-    data['username'] = self.user.username
-    data['email'] = self.user.email
+    # data['username'] = self.user.username
+    # data['email'] = self.user.email
+
+    serializer = UserSerializerWithToken(self.user).data
+
+    for key, value in serializer.items():
+      data[key] = value
     
     return data
 
