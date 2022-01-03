66f23014d74a2650a2858f8b9b0f9359ae3ebfdc
Brian Parker <btparker70@gmail.com>
2022-01-02 16:48:05 -0800

    added isadmin permission class to the get request for all user data

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
+from django.contrib.auth.models import User
 from .models import Product
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
@@ -58,6 +59,13 @@ def getRoutes(req):
 def getUserProfile(req):
   user = req.user
   serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def getUsers(req):
+  users = User.objects.all()
+  serializer = UserSerializer(users, many=True)
   return Response(serializer.data)
 
 @api_view(['GET'])
