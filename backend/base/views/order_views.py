1ea172f776dced091bd466eae278322fa0179e0f
Brian Parker <btparker70@gmail.com>
2022-02-05 23:04:44 -0800

    build out order views

---
+++ backend/base/views/order_views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+from base.models import Product, Order, OrderItem, ShippingAddress
+from base.serializers import ProductSerializer
+
+from rest_framework import status
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(req):
+  user = req.user
+  data = request.data
+
+  orderItems = data['orderItems']
+
+  if orderItems and len(orderItems) == 0:
+    return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+  else: 
+    # (1) create order
+    order = Order.objects.create(
+      user=user,
+      paymentMethod=data['paymentMethod'],
+      taxPrice=data['taxPrice'],
+      shippingPrice=data['shippingPrice'],
+      totalPrice=data['totalPrice']
+    )
+    # (2) create shipping address
+    # (3) create order items and set order to orderitem relationship
+    # (4) update stock
+
+  return Response('ORDER')
