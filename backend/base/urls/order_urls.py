2d2a49d60d3e7fa443b311b48551a66b519a77b2
Brian Parker <btparker70@gmail.com>
2022-02-16 22:30:48 -0800

    added method to get order by order id. fixed shipping address error on get shipping serializer

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -6,4 +6,5 @@ from base.views import order_views as views
 # when you go to these routes, load these views
 urlpatterns = [
   path('add/', views.addOrderItems, name='orders-add'),
+  path('<str:pk>/', views.getOrderById, name='user-order'),
 ]
